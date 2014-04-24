117a77d59b254b19b3f7e4cf78dc06a3b9b13996
Sergey Homenkow <sergey@homenkow.ru>
2014-04-25 03:11:57 +0400

    use chrome.runtime.connect(). throttle messages, icon updates.

--- bg.js
+++ bg.js
@@ -1,18 +1,19 @@
 "use strict";
 
-var icon_size = 19,
+var icon_size = 19 * window.devicePixelRatio,
     eye_radius = Math.round(icon_size / 5),
     pupil_radius = Math.round(eye_radius / 2),
     max_x_delta = icon_size - 4 * eye_radius,
     max_y_delta = icon_size - 2 * eye_radius,
-    max_pupil_delta = eye_radius - pupil_radius;
+    max_pupil_delta = eye_radius - pupil_radius,
+    time = window.performance.now();
 
 var canvas = document.createElement('canvas');
 canvas.width = icon_size;
 canvas.height = icon_size;
 var ctx = canvas.getContext('2d');
 
-function showEyes(x, y) {
+function showEyes(x, y, sleep) {
     ctx.clearRect(0, 0, icon_size, icon_size);
 
     var eye_x = icon_size - eye_radius - max_x_delta * x,
@@ -23,14 +24,12 @@ function showEyes(x, y) {
     for(var i = 0; i < 2; i++) {
         var eye_xx = eye_x - i * (2 * eye_radius - 1);
 
-        ctx.strokeStyle = "black";
-        ctx.fillStyle = "black";
+        ctx.strokeStyle = ctx.fillStyle = "black";
         ctx.beginPath();
         ctx.arc(eye_xx, eye_y, eye_radius, 0, Math.PI * 2);
         ctx.stroke();
 
-        ctx.strokeStyle = "red";
-        ctx.fillStyle = "red";
+        ctx.strokeStyle = ctx.fillStyle = sleep ? "grey" : "red";
         ctx.beginPath();
         ctx.arc(eye_xx - pupil_delta_x, eye_y + pupil_delta_y, pupil_radius, 0, Math.PI * 2);
         ctx.fill();
@@ -42,11 +41,22 @@ function showEyes(x, y) {
     });
 }
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        var x = request.x > 1 ? 1 : request.x,
-            y = request.y > 1 ? 1 : request.y;
+function processRequest(request) {
+    var x = Math.min(request.x, 1),
+        y = Math.min(request.y, 1),
+        t2 = window.performance.now();
+    if (t2 - time > 1/60) {
+        time = t2;
         showEyes(x, y);
-        sendResponse({ok:1});
     }
-);
+}
+
+chrome.runtime.onConnect.addListener(function(port) {
+    port.onMessage.addListener(processRequest);
+});
+
+chrome.runtime.onSuspend.addListener(function() {
+    showEyes(0, 0, true);
+});
+
+showEyes(0, 0, true);
--- cs.js
+++ cs.js
@@ -1,14 +1,19 @@
 "use strict";
 
-var process = false;
+var port = chrome.runtime.connect({name: "mousemove"}),
+    old_x = -1,
+    old_y = -1;
+
 document.addEventListener('mousemove', function (e) {
-    if (!process) {
-        process = true;
-        chrome.runtime.sendMessage({
-                x: 1 - (e.clientX / window.innerWidth),
-                y: e.clientY / window.innerHeight
-                }, function() {
-            process = false;
+    var x = 1 - (e.clientX / window.innerWidth),
+        y = e.clientY / window.innerHeight;
+
+    if (Math.abs(x - old_x) + Math.abs(y - old_y) > 0.01) {
+        old_x = x;
+        old_y = y;
+        port.postMessage({
+            x: x,
+            y: y
         });
     }
 }, false);
