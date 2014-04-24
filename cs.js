bff5414acf2f8ef9af865684adc8541819b5b0d7
Sergey Homenkow <sergey@homenkow.ru>
2014-04-25 00:35:15 +0400

    event.pageY => event.client*

--- cs.js
+++ cs.js
@@ -5,9 +5,9 @@ document.addEventListener('mousemove', function (e) {
     if (!process) {
         process = true;
         chrome.runtime.sendMessage({
-                x: 1 - (e.pageX / window.innerWidth),
-                y: e.pageY / window.innerHeight
-                }, function(response) {
+                x: 1 - (e.clientX / window.innerWidth),
+                y: e.clientY / window.innerHeight
+                }, function() {
             process = false;
         });
     }
